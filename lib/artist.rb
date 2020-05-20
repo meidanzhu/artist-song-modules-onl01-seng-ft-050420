66d1700e44d60dd27a1db3d9f9a75a6fcd22f9b3
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-20 02:56:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,12 @@
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
   attr_accessor :name
   attr_reader :songs
-
   @@artists = []
 
   def initialize

6f62c12b1883fefe8ab1d447f724c6cd179d4160
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-20 02:59:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,7 @@
 require 'pry'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/paramable'
+require_relative '../lib/concerns/findable'
 
 class Song
   extend Memorable::ClassMethods

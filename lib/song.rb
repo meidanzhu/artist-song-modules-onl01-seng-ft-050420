a398fc6ce0c7ac3156b28e0ba8fdaf2ed40b9198
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-20 02:57:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,11 @@
 require 'pry'
 
 class Song
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
+  
   attr_accessor :name
   attr_reader :artist
 

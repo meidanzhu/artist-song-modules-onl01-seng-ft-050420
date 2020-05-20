6ad810999e73ed15887a9fe93542429592cbebef
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-20 03:04:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 class Artist
+
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable
@@ -8,27 +9,16 @@ class Artist
 
   attr_accessor :name
   attr_reader :songs
+
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
   end
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -38,9 +28,5 @@ class Artist
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 end

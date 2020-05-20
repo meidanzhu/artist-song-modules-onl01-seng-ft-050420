324103803ea3877a5d2bf1e661b24bc062bb2c67
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-20 03:02:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,26 +1,26 @@
-
 require 'pry'
+
 
 require_relative '../lib/concerns/memorable'
 require_relative '../lib/concerns/paramable'
 require_relative '../lib/concerns/findable'
 
-class Song
-extend Memorable::ClassMethods
-include Memorable::InstanceMethods
-include Paramable::InstanceMethods
-extend Findable::ClassMethods
+class Artist
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable
+  extend Findable
 
-attr_accessor :name
-attr_reader :artist
+  attr_accessor :name
+  attr_reader :artist
 
-@@songs = []
+  @@songs = []
 
-def self.all
-  @@songs
-end
+  def self.all
+    @@songs
+  end
 
-def artist=(artist)
-  @artist = artist
-end
+  def artist=(artist)
+    @artist = artist
+  end
 end

d774592124a50ba265c214e62134c7db39382ecb
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-20 03:05:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,8 @@
 require 'pry'
 
 
-require_relative '../lib/concerns/memorable'
-require_relative '../lib/concerns/paramable'
-require_relative '../lib/concerns/findable'
+class Song
 
-class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable
